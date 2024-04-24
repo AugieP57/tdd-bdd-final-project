28f6b58329a0d7d05295df88070cf1faf843c2a3
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2024-04-24 13:02:35 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -130,6 +130,22 @@ def list_products():
 ######################################################################
 # List By Name
 ######################################################################
+@app.route("/name", methods=["GET"])
+def test_query_by_name(self):
+        """It should Query Products by name"""
+        products = self._create_products(5)
+        test_name = products[0].name
+        name_count = len([product for product in products if product.name == test_name])
+        response = self.client.get(
+            BASE_URL, query_string=f"name={quote_plus(test_name)}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), name_count)
+        # check the data just to be sure
+        for product in data:
+            self.assertEqual(product["name"], test_name)
+            
 ######################################################################
 # List By Category
 ######################################################################
